082d013e91775ee70b70d38d4c8a8897684b862c
RHenrique <roosevelthenrique22@gmail.com>
2022-12-23 01:17:43 -0300

    [feat]: Implementados testes unitários

--- api/app/models/comentario.rb
+++ api/app/models/comentario.rb
@@ -1,3 +1,7 @@
 class Comentario < ApplicationRecord
   has_many :comentarios
+
+  validates :mensagem ,  presence: true , length: { in: 2..500 }
+  validates :like     ,  presence: true , numericality: { numericality: true, only_integer: true }
+  validates :deslike  ,  presence: true , numericality: { numericality: true, only_integer: true }
 end
--- api/spec/models/comentario_spec.rb
+++ api/spec/models/comentario_spec.rb
@@ -1,5 +1,157 @@
 require 'rails_helper'
 
 RSpec.describe Comentario, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let( :comentario ){ Comentario.new }
+
+  describe "Validar atributos" do
+
+    it "Validar se a mensagem não está presente" do
+
+      comentario.like       = 1
+      comentario.deslike    = 0
+  
+      expect(comentario).not_to be_valid
+
+    end
+
+    it "Validar tamanho mínimo da mensagem" do
+
+      comentario.mensagem   = "a"
+      comentario.like       = 1
+      comentario.deslike    = 0
+  
+      expect(comentario).not_to be_valid
+
+    end
+  
+    it "Validar se a mensagem está presente" do
+
+      comentario.mensagem   = "Teste"
+      comentario.like       = 1
+      comentario.deslike    = 0
+  
+      expect(comentario).to be_valid
+
+    end
+
+    it "Validar se o like não está presente" do
+
+      comentario.mensagem   = "Teste"
+      comentario.deslike    = 0
+  
+      expect(comentario).not_to be_valid
+
+    end
+
+    it "Validar se o like está presente" do
+
+      comentario.mensagem   = "Teste"
+      comentario.like       = 1
+      comentario.deslike    = 0
+  
+      expect(comentario).to be_valid
+
+    end
+
+    it "Validar se o like não está presente" do
+
+      comentario.mensagem   = "Teste"
+      comentario.like       = 0
+  
+      expect(comentario).not_to be_valid
+
+    end
+
+    it "Validar se o like está presente" do
+
+      comentario.mensagem   = "Teste"
+      comentario.like       = 1
+      comentario.deslike    = 0
+  
+      expect(comentario).to be_valid
+
+    end
+
+    it "Validar se o like não é númerico" do
+
+      comentario.mensagem   = "Teste"
+      comentario.like       = "a"
+      comentario.deslike    = 0
+  
+      expect(comentario).not_to be_valid
+
+    end
+
+    it "Validar se o like é númerico" do
+
+      comentario.mensagem   = "Teste"
+      comentario.like       = 1
+      comentario.deslike    = 0
+  
+      expect(comentario).to be_valid
+
+    end
+
+    it "Validar se o like é númerico, mas não inteiro" do
+
+      comentario.mensagem   = "Teste"
+      comentario.like       = 1.2
+      comentario.deslike    = 0
+  
+      expect(comentario).not_to be_valid
+
+    end
+
+    it "Validar se o like é númerico e inteiro" do
+
+      comentario.mensagem   = "Teste"
+      comentario.like       = 1
+      comentario.deslike    = 0
+  
+      expect(comentario).to be_valid
+
+    end
+
+    it "Validar se o deslike não é númerico" do
+
+      comentario.mensagem   = "Teste"
+      comentario.like       = 1
+      comentario.deslike    = "a"
+  
+      expect(comentario).not_to be_valid
+
+    end
+
+    it "Validar se o deslike é númerico" do
+
+      comentario.mensagem   = "Teste"
+      comentario.like       = 1
+      comentario.deslike    = 0
+  
+      expect(comentario).to be_valid
+
+    end
+
+    it "Validar se o deslike é númerico, mas não inteiro" do
+
+      comentario.mensagem   = "Teste"
+      comentario.like       = 1
+      comentario.deslike    = 1.2
+  
+      expect(comentario).not_to be_valid
+
+    end
+
+    it "Validar se o deslike é númerico e inteiro" do
+
+      comentario.mensagem   = "Teste"
+      comentario.like       = 1
+      comentario.deslike    = 0
+  
+      expect(comentario).to be_valid
+
+    end
+
+  end
+
 end
